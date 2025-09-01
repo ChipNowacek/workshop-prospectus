e913aa4643532d907a7c4052998005cc6dc3d715
Chip Nowacek <chip@twostewards.com>
2025-09-01 08:31:42 -0500

    Create current-processing-script.sh
    bibliography generation is working

---
+++ current-processing-script.sh
@@ -0,0 +1,7 @@
+# Produce the PDF file and clean up
+latexmk -bibtex -silent -interaction=nonstopmode -xelatex -jobname="$2" "$1"
+latexmk -c "$2.pdf"
+
+# With all of the above dancing around, Scrivener won't know
+# this is what we really want to open...
+open "$2.pdf"
